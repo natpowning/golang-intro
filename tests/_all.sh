50aa8d0bba1a150df4e67f998abf20bdd232ad93
Nat Powning <nat.powning@trustvesta.com>
2019-10-18 11:41:35 -0700

    Iterating on multiple test suites in the hokee bash form.

--- tests/_all.sh
+++ tests/_all.sh
@@ -2,8 +2,9 @@ DIR=`dirname "$0"`
 
 echo "Executing All Tests..."
 
-$DIR/smoke.sh
-$DIR/user_lifecycle.sh
+# TODO: Handle failure exit codes
+bash $DIR/smoke.sh
+bash $DIR/user_lifecycle.sh
 
 echo "---"
 echo "All Tests Complete"
